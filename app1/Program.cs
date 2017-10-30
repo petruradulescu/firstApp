4d6fc071ca43273f3837f7ec0cef249ad12f887d
Radulescu Petru <radulescupetru.v@outlook.com>
2017-10-30 20:22:17 +0200

    Added console app

---
+++ app1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("asdasda");
+        }
+    }
+}
